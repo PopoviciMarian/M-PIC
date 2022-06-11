57095dbe387f194dcd6c3bce67720fe5608b9851
BITDEFENDER\mapopovici <mapopovici@bitdefender.com>
2022-06-12 01:32:47 +0300

    + cors

--- Backend/AuthorizationService/index.js
+++ Backend/AuthorizationService/index.js
@@ -16,8 +16,16 @@ pool.query("SELECT NOW()")
 const NAMESPACE = 'Server';
 const port = process.env.SERVER_PORT;
 function serverFunc(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    if ( req.method === 'OPTIONS' ) {
+		res.writeHead(200);
+		res.end();
+		return;
+    }
     logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
-
     router.callRoute(req, res);
     res.on('finish', () => {
         logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
