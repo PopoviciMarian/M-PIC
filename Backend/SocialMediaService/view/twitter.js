60a3c78367c69fff6b5c215c27d1ba83f9639d02
Denis Anicai <denisanicai14@gmail.com>
2022-06-20 03:28:11 +0300

    Get TWITTER images, likes, reply_count

--- Backend/SocialMediaService/view/twitter.js
+++ Backend/SocialMediaService/view/twitter.js
@@ -10,6 +10,48 @@ class TwitterView{
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify({message: content['message']}));
     }
+    getTwitterImages(res, content){
+        var newResult = [];
+        
+        if(content['code'] === 200){
+            for(var i = 0; i < content.message['meta']['result_count']; i++){
+                var url = 'https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.1920.jpg'
+                var media_key;
+                var hasPhoto = true;
+                try {
+                    media_key = content.message['data'][i]['attachments']['media_keys'];
+                }
+                catch{
+                    hasPhoto = false;
+                }
+
+                if(hasPhoto === true)
+                {
+                    for(var j = 0; j < content.message['meta']['result_count']; j++)
+                    {
+                        if(content.message['includes']['media'][j]['media_key'] == media_key) {
+                            url = content.message['includes']['media'][j]['url'];
+                            break;
+                        }
+                    }
+                }
+                
+                const obj = {
+                    text: content.message['data'][i]['text'],
+                    public_metrics: content.message['data'][i]['public_metrics'],
+                    url
+                    
+                    
+                }
+                newResult.push(obj);
+            }
+            content.message = newResult;
+        }
+
+        res.statusCode = content['code'];
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({message: content['message']}));
+    }
 }
 
 let view = new TwitterView();
